8b3b63e0f18258aaf1749c4db3c426457699ed87
SNunesA <snunesadiers@gmail.com>
2024-11-07 20:37:52 -0300

    album

--- projeto/spotify/models.py
+++ projeto/spotify/models.py
@@ -6,3 +6,12 @@ class Artista(models.Model):
     genero=models.CharField(null=True,max_length=200) #biografia pode ser nula
     def __str__(self):
         return self.nome
+    
+class Album(models.Model):
+    titulo = models.CharField(max_length=255, unique=True)
+    ano_publicacao = models.IntegerField()
+    descricao = models.TextField(blank=True, null=True)
+    artista = models.ForeignKey(Artista, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.titulo
--- projeto/spotify/serializers.py
+++ projeto/spotify/serializers.py
@@ -1,7 +1,25 @@
 from rest_framework import serializers
-from .models import Artista
+from .models import Artista, Album
 
 class ArtistaSerializer(serializers.ModelSerializer):
     class Meta:
         model=Artista
         fields='__all__'
+
+
+class AlbumArtistaerializer(serializers.ModelSerializer):
+    artista = ArtistaSerializer()
+
+    class Meta:
+        model = Album
+        fields = ['id', 'titulo', 'ano_publicacao', 'descricao', 'artista']
+
+    
+    def create(self, validated_data):
+        artista_data = validated_data.pop('artista')
+        artista, created = Artista.objects.get_or_create(**artista_data)
+
+        # Criar o album associado ao artista
+        album = Album.objects.create(artista=artista, **validated_data)
+
+        return album
